bb54df1fc95f7c57910558753487d8a6abca8f88
alex-lee13 <122317975+alex-lee13@users.noreply.github.com>
2023-02-15 22:16:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,46 +4,40 @@ Prof. Mosca
 Modified: 02/15/2023
 */
 
+
 // Declare constants
 
-const FRAME_HEIGHT = 200;
-const FRAME_WIDTH = 500;
-const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
+const margin = { top: 20, right: 30, bottom: 50, left: 50 };
+const frame_width = 500 - margin.left - margin.right;
+const frame_height = 500 - margin.top - margin.bottom;
+
+// Append the svg object to the body of the page
+const frame = d3.select("#frame_div")
+              .append("svg")
+              .attr("width", frame_width + margin.left + margin.right)
+              .attr("height", frame_height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 const data = [55000, 48000, 27000, 66000, 90000];
 
 
-const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
-const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
 
-const FRAME = d3.select("#frame_div")
-                  .append("svg")
-                    .attr("height", FRAME_HEIGHT)
-                    .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame"); 
+var x_scale = d3.scaleBand()
+                  .range([0, frame_width])
+                  .padding(0.1)
+                  .domain(data.map(function(d, i) { return i; }));
 
-// Now, let's define our scaling function
-
-// find max X
-const MAX_Y = d3.max(data, (d) => { return d; }); 
-console.log("Max y: " + MAX_Y);  
-
-// Now, define scale functions that maps our data values 
-// (domain) to pixel values (range)
-const Y_SCALE = d3.scaleLinear() // linear scale because we have 
-                              // linear data 
-                  .domain([0, (MAX_Y + 10000)]) // add some padding  
-                  .range([0, VIS_WIDTH]); 
-
-console.log("Input: 90000, Y_SCALE output: " + Y_SCALE(90000));
-
-// Now, we can use X_SCALE to plot our points
-FRAME.selectAll("points")  
-    .data(data)  
-    .enter()       
-    .append("circle")  
-      .attr("cx", MARGINS.left) 
-      .attr("cy", (d) => { return (Y_SCALE(d) + MARGINS.top)}) 
-      .attr("r", 20)
-      .attr("class", "point");
-
+var y_scale = d3.scaleLinear()
+                  .domain([0, 100000])
+                  .range([frame_height, 0]);
+                  
+// Add dots
+frame.selectAll("points")
+  .data(data)
+  .enter()
+  .append("circle")
+    .attr("cx", function(d, i) { return x(i); })
+    .attr("cy", function(d) { return y(d); })
+    .attr("r", 5)
+    .style("fill", 'blue');
